bb6e925543f1fcd2e0e2e61a9ed26ee8222afd80
Flatiron School <email.the.anderson@gmail.com>
2019-12-09 21:28:56 -0500

    hurfardff

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+//hurfadurff
 function Die({img, dx, rollDice, addToRollBox}) {
     let seedrandom = require('seedrandom');
 
