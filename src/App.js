e38042810a3350ad770d71410a5d1228365838e7
Flatiron School <email.the.anderson@gmail.com>
2019-12-31 10:59:27 -0500

    created Navbar component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import DiceContainer from './components/DiceContainer.js';
+import NavBar from './components/NavBar.js'
 
 function App() {
   return (
     <div className='App'>
       <div id="navbar" className='navbar-item'>
-        this is a navbar
+        {/* this is a navbar */}
+        <NavBar/>
       </div>
       <DiceContainer/>
       {/* <RollBox/> */}
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+function NavBar() {
+
+    return(
+        "This is a navbar"
+    )
+}
+
+export default NavBar
